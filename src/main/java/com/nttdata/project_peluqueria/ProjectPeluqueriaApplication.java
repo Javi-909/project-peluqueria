921ca50c81c5e203048830d719da6d81d18522c9
Javi-909 <javiquevedo09@gmail.com>
2025-10-14 11:12:56 +0200

    First commit

---
+++ src/main/java/com/nttdata/project_peluqueria/ProjectPeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.project_peluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPeluqueriaApplication.class, args);
+	}
+
+}
